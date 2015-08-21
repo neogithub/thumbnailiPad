a37ad6a8196e0bb23545a45bf1949fd8a161982d
Xiaohe <huxiaohe113@gmail.com>
2015-08-20 20:57:50 -0400

    Initial Commit

---
+++ thumbnailiPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  thumbnailiPad
+//
+//  Created by Xiaohe Hu on 8/20/15.
+//  Copyright (c) 2015 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ thumbnailiPad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thumbnailiPad
+//
+//  Created by Xiaohe Hu on 8/20/15.
+//  Copyright (c) 2015 Xiaohe Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
